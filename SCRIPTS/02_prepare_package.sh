95e02a680b1c12debe78c849948f0d0e360a5897
zidbox <61278966+zidbox@users.noreply.github.com>
2020-05-18 22:26:12 +0800

    Update 02_prepare_package.sh

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -110,5 +110,6 @@ git clone -b master --single-branch https://github.com/QiuSimons/addition-trans-
 rm -rf .config
 #修正架构
 sed -i "s,boardinfo.system,'ARMv8',g" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 chmod -R 755 ./
 exit 0
